12b4c1fc6d3ac7d0d1143205d51e25296a5f8c60
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-12-20 22:33:10 +0100

    add the erd and readme.md and sql

--- SQL.sql
+++ SQL.sql
@@ -8,14 +8,29 @@ adress text,
 tel text,
 email varchar(50) unique,
 password text,
-role enum('admin','user') Default 'user'
+RoleId int ,
+foreign key(RoleId) references Role(id) on delete cascade
 );
-insert into client(nom,prenom,adress,tel,email,password,role) values("Zemmari","azzedine","Hey el massira LOTIS 500","0622328719","Azzedine@gmail.com","Azzedine2004","admin");
 select * from client;
+SELECT (count(*) - 1) as total  from client;
+create table Role(
+id int not null PRIMARY KEY AUTO_INCREMENT,
+titre varchar(20)
+);
+insert into role(titre) values("admin");
+select * from role;
+insert into client(nom,prenom,adress,tel,email,password,RoleId) values("Zemmari","azzedine","Hey el massira LOTIS 500","0622328719","Azzedine@gmail.com","Azzedine2004",1);
+select * from menu;
+delete from menu where id = 5;
 create table menu (
 id int not null PRIMARY KEY AUTO_INCREMENT,
 nom varchar(20)
 );
+insert into menu(nom) values("breakfast");
+insert into menu(nom) values("lunch");
+insert into menu(nom) values("dinner");
+select * from Plat;
+select * from menu;
 drop table Plat;
 create table Plat (
 id int not null PRIMARY KEY AUTO_INCREMENT,
@@ -25,6 +40,19 @@ menuId int,
 image text,
 FOREIGN KEY (menuId) REFERENCES menu(id) on delete cascade
 );
+insert into Plat(nom,ingrediant,menuId,image) values("salde nisoise","les pats , sourice , fromage , mayounaise",2,"./image/img1.png");
+insert into Plat(nom,ingrediant,menuId,image) values("poulet roti avec de riz","poulet , riz , legume souter",2,"./image/img1.png");
+insert into Plat(nom,ingrediant,menuId,image) values("jus de pomme","pomme , banane , ...",2,"./image/img1.png");
+
+insert into Plat(nom,ingrediant,menuId,image) values("dejauner marocain"," , oumlette avec .. , fromage , thé",1,"./image/img1.png");
+insert into Plat(nom,ingrediant,menuId,image) values("dejauner american ","dinde fumme , oumlette avec .. , legume souter , viande hache",1,"./image/img1.png");
+insert into Plat(nom,ingrediant,menuId,image) values("italian","cafe , pan au chocolat  , ...",1,"./image/img1.png");
+
+SELECT menu.nom , menu.id , Plat.nom,Plat.ingrediant,Plat.image from Plat 
+join menu on menu.id = Plat.menuId;
+
+
+
 drop table Reservation;
 create table Reservation(
 id int not null PRIMARY KEY AUTO_INCREMENT,
@@ -33,7 +61,29 @@ MenuId int,
 dateReservation date,
 heur TIME,
 nbrPerson int,
-status enum ('Anuller','Attent','Confirme'),
+status enum ('Anuller','Attent','Confirme') DEFAULT 'Attent',
 foreign key(clientId) references client(id) on delete cascade,
 foreign key(MenuId) references menu(id)  on delete cascade
-)
+);
+insert into Reservation(clientId,MenuId,dateReservation,heur,nbrPerson) values(3,2,"2024-12-18","12:00",12);
+select * from Reservation;
+
+select client.nom as ClientNom , Reservation.id , menu.nom as MenuNom , Reservation.dateReservation , Reservation.heur , Reservation.nbrPerson , Reservation.status from Reservation 
+            join client on client.id = Reservation.clientId 
+            join menu on Reservation.MenuId = menu.id;
+            
+select client.nom as ClientNom , Reservation.id , menu.nom as MenuNom , Reservation.dateReservation , Reservation.heur , Reservation.nbrPerson , Reservation.status , Reservation.clientId , Reservation.MenuId from Reservation 
+            join client on client.id = Reservation.clientId 
+            join menu on Reservation.MenuId = menu.id;
+            
+select Reservation.MenuId , menu.nom from menu 
+join Reservation on Reservation.MenuId = menu.id;
+
+
+SELECT count(*) from Reservation where status = 'Attent';
+
+
+SELECT  menu.nom as MenuName, Plat.nom as PlatName, Plat.ingrediant 
+    FROM Plat 
+    JOIN menu ON menu.id = Plat.menuId 
+    ORDER BY menu.nom;
